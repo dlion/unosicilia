5208bc103998f9267929c19dce104a0c10f7e747
Domenico Leone Luciani <domenicoleoneluciani@gmail.com>
2014-03-05 21:00:48 +0100

    added roba

---
+++ js/grayscale.js
@@ -0,0 +1,36 @@
+//jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('.page-scroll a').bind('click', function(event) {
+        var $anchor = $(this);
+
+        /*
+        $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+          event.preventDefault();
+        */
+        $(".main").jumpTo($anchor.data("target"));
+    });
+
+    $(".main").onepage_scroll({
+      sectionContainer: "section",
+      easing: "ease",
+      animationTime: 1000,
+      pagination: false,
+      loop: false,
+      keyboard: true,
+      responsiveFallback: true
+    });
+
+});
+
