85af1570ec6d89185ce33190e2654e2603ef3c48
Domenico Leone Luciani <domenicoleoneluciani@gmail.com>
2014-03-16 16:55:40 +0100

    Desktop responsive

--- js/main.js
+++ js/main.js
@@ -1,29 +1,33 @@
 $(document).ready(function() {
+  // Configurazione del plugin fullPage
   $.fn.fullpage({
     anchors: ["Home", "Certificazioni", "Calendario", "Prodotti", "Aziende", "Contatti"],
     slidesColor: ['#FFF', '#FFF', '#FFF'],
     css3: true,
     fixedElements: "header"
   });
+
+  // Prendo il pull e il menu
   var pull = $("#pull"),
       menu = $("nav ul");
 
+  // Se clicco il pull
   $(pull).on("click", function(e) {
     e.preventDefault();
     menu.slideToggle();
   });
 
-  $(window).resize(function() {
-    var w = $(this).width();
-
-    if (w > 360 && menu.is(":hidden")) {
-      menu.removeAttr("style");
+  $(window).resize(function(){
+    var w = $(window).width();
+    if(w > 320 && menu.is(':hidden')) {
+      menu.removeAttr('style');
     }
   });
 
+  // Se sono da mobile quando clicco su un li leva il menu
   $("li").on("click", function(e) {
     var w = $(window).width();
-    if ( w < 360) {
+    if ( w <= 360) {
       menu.slideToggle();
     }
   });
