a9401088d515668e0871503ecd15298365eb50ca
Domenico Leone Luciani <domenicoleoneluciani@gmail.com>
2014-03-17 01:03:53 +0100

    Fixed responsive

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@ $(document).ready(function() {
   // Se sono da mobile quando clicco su un li leva il menu
   $("li").on("click", function(e) {
     var w = $(window).width();
-    if ( w <= 600) {
+    if ( w <= 480) {
       menu.slideToggle();
     }
   });
