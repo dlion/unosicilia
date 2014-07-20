8c129f9f9551f7d6e1ed8a2afbfd3da6e9e9d1e1
Domenico Leone Luciani <domenicoleoneluciani@gmail.com>
2014-07-21 00:41:54 +0200

    Init

--- js/main.js
+++ js/main.js
@@ -1,34 +1,60 @@
 $(document).ready(function() {
+  /*
+   * Se premi un pulsante tolgo l'active e lo metto in quello selezionato
+   */
+   $("li[data-menuanchor='Home']").click(function() {
+     $("li[data-menuanchor='Home']").addClass("active");
+   });
+
+   $("li[data-menuanchor='Prodotti']").click(function() {
+     $("li[data-menuanchor='Prodotti']").addClass("active");
+   });
+
+   $("li[data-menuanchor='Produttori']").click(function() {
+     $("li[data-menuanchor='Produttori']").addClass("active");
+   });
+
+   $("li[data-menuanchor='Calendario']").click(function() {
+     $("li[data-menuanchor='Calendario']").addClass("active");
+   });
+
+   $("li[data-menuanchor='Certificazioni']").click(function() {
+     $("li[data-menuanchor='Certificazioni']").addClass("active");
+   });
+
+   $("li[data-menuanchor='Contatti']").click(function() {
+     $("li[data-menuanchor='Contatti']").addClass("active");
+   });
+
   // Configurazione del plugin fullPage
   $.fn.fullpage({
-    anchors: ["Home", "Certificazioni", "Calendario", "Prodotti", "Aziende", "Contatti"],
-    slidesColor: ['#FFF', '#FFF', '#FFF'],
+    anchors: ["Home", "Prodotti", "Produttori", "Calendario", "Certificazioni", "Contatti"],
+    slidesColor: ['#FFF', '#FFF', '#FFF', '#FFF', '#FFF', '#FFF'],
     css3: true,
-    fixedElements: "header"
-  });
-
-  // Prendo il pull e il menu
-  var pull = $("#pull"),
-      menu = $(".header > ul");
-
-  // Se clicco il pull
-  $(pull).on("click", function(e) {
-    e.preventDefault();
-    menu.slideToggle();
-  });
-
-  $(window).resize(function(){
-    var w = $(window).width();
-    if(w > 320 && menu.is(':hidden')) {
-      menu.removeAttr('style');
-    }
-  });
-
-  // Se sono da mobile quando clicco su un li leva il menu
-  $(".header > ul > li > a").on("click", function(e) {
-    var w = $(window).width();
-    if ( w <= 480) {
-      menu.slideToggle();
-    }
+    fixedElements: "header",
+    verticalCentered: true,
+    animateAnchor: true,
+     onLeave: function(index, nextIndex, direction){
+       switch(index) {
+         case 1:
+         $("li[data-menuanchor='Home']").removeClass("active");
+          break;
+        case 2:
+           $("li[data-menuanchor='Prodotti']").removeClass("active");
+          break;
+        case 3:
+           $("li[data-menuanchor='Produttori']").removeClass("active");
+          break;
+        case 4:
+           $("li[data-menuanchor='Calendario']").removeClass("active");
+          break;
+        case 5:
+           $("li[data-menuanchor='Certificazioni']").removeClass("active");
+          break;
+        case 6:
+           $("li[data-menuanchor='Contatti']").removeClass("active");
+          break;
+       }
+     }
   });
 });
