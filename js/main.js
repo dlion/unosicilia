043b79afda08fe828ca1f0aeb6c3fe85079d0e5d
Domenico Leone Luciani <domenicoleoneluciani@gmail.com>
2014-12-11 23:16:40 +0100

    Rimosso unslider

--- js/main.js
+++ js/main.js
@@ -68,7 +68,7 @@ $('#fullpage').fullpage({
       doveSono.removeClass("scrittaAttiva");
       $(this).addClass("scrittaAttiva");
       doveSono = $(this);
-   //   banneroneFrutta.to(0); TODO: Qui ci deve andare il cambio di immagine
+      $("#ImmagineSection1").attr("src","./materiale/TRASFORMATI/FRUTTA.png");
       titoloBoxPage2.text("Frutta");
       testoFrescoBoxPage2.text("Commercializziamo una vasta gamma di frutta di stagione coltivata esclusivamente da aziende agricole siciliane ed immessa nel mercato dopo essere stata accuratamente selezionata. Grazie alle caratteristiche uniche del nostro territorio, all' esperienza e alla sapienza dei nostri agricoltori, otteniamo prodotti di grande qualità che ripropongono i sapori tipici della nostra terra.");
     });
@@ -77,7 +77,7 @@ $('#fullpage').fullpage({
       doveSono.removeClass("scrittaAttiva");
       $(this).addClass("scrittaAttiva");
       doveSono = $(this);
-      //banneroneFrutta.to(1); TODO: Qui ci deve andare il cambio di immagine
+      $("#ImmagineSection1").attr("src","./materiale/TRASFORMATI/OLIVE_OLIO.png");
       titoloBoxPage2.text("Olive e Olio");
       testoFrescoBoxPage2.text("La Sicilia è una delle regioni con il maggior numero di varietà di olivo di tutto il bacino del Mediterraneo, molte di queste sono varietà autoctone di antichissima coltivazione ognuna delle quali ha una propria zona di vocazione e da cui si ricavano oli di oliva con rese elevate e di ottima qualità.");
     });
@@ -86,7 +86,7 @@ $('#fullpage').fullpage({
       doveSono.removeClass("scrittaAttiva");
       doveSono = $(this);
       doveSono.addClass("scrittaAttiva");
-    //  banneroneFrutta.to(2); TODO: Qui ci deve andare il cambio di immagine
+      $("#ImmagineSection1").attr("src","./materiale/TRASFORMATI/AGRUMI.png");
       titoloBoxPage2.text("Agrumi");
       testoFrescoBoxPage2.text("Gli agrumi, frutti dall'inconfondibile profumo, rientrano a pieno titolo nell'elenco dei prodotti che rappresentano non solo l'agricoltura siciliana ma anche uno dei nostri prodotti di punta. Le loro caratteristiche organolettiche ed il loro alto contenuto di vitamina C li rendono un alimento utilissimo per la salute umana.");
     });
@@ -95,7 +95,7 @@ $('#fullpage').fullpage({
       doveSono.removeClass("scrittaAttiva");
       doveSono = $(this);
       doveSono.addClass("scrittaAttiva");
-    //  banneroneFrutta.to(3); TODO: Qui ci deve andare il cambio di immagine
+      $("#ImmagineSection1").attr("src","./materiale/TRASFORMATI/ORTAGGI.png");
       titoloBoxPage2.text("Ortaggi");
       testoFrescoBoxPage2.text("Puntiamo sulla ricerca e sull' innovazione per mettere in atto metodi che ci permettano di migliorare le colture che produciamo e che caratterizzano la nostra regione, rispondendo alle esigenze del consumatore finale offrendo prodotti sani, gustosi e di alta qualità, nati solo ed esclusivamente da scelte consapevoli a sostegno dell' ambiente. Monitoriamo ogni fase della produzione, dal campo fino al consumatore finale, proponendo una vasta gamma di ortaggi di stagione in piena conformità agli standard di certificazione richiesti dal settore.");
     });
@@ -112,7 +112,7 @@ $('#fullpage').fullpage({
       doveSono2.removeClass("scrittaAttiva");
       doveSono2 = $(this);
       doveSono2.addClass("scrittaAttiva");
-      //banneroneProduttori.to(0); TODO: Qui ci deve andare il cambio di immagine
+      $("#ImmagineSection2").attr("src","./materiale/TRASFORMATI/TERRA_DELLE_ROSE.png");
       titoloBoxProduttori.text("Azienda agricola Terra delle Rose");
       testoBoxProduttori.text("L'azienda si trova in Contrada Rossella, SNC a Piana degli Albanesi. La sua attività è la coltivazione di frutti oleosi.");
     });
@@ -121,7 +121,7 @@ $('#fullpage').fullpage({
       doveSono2.removeClass("scrittaAttiva");
       doveSono2 = $(this);
       doveSono2.addClass("scrittaAttiva");
-     // banneroneProduttori.to(1); TODO: Qui ci deve andare il cambio di immagine
+      $("#ImmagineSection2").attr("src","./materiale/TRASFORMATI/BOSCO_FICUZZA.png");
       titoloBoxProduttori.text("Azienda agricola Bosco Ficuzza");
       testoBoxProduttori.text("La Bosco Ficuzza azienda agricola biologica s.c. si trova su due colline, Pero e Cannatino, in territorio di Cerda (Pa) alle falde della catena montuosa delle Madonie. Si converte al biologico nel 2001, preoccupandosi di ridurre al minimo gli impatti delle attività agricole dannose per l'ambiente, riducendo gli apporti di sostanze chimiche e assicurando un approccio responsabile nei confronti della salubrità e la sicurezza dei lavoratori e del benessere degli animali.");
     });
@@ -137,7 +137,7 @@ $('#fullpage').fullpage({
       doveSono3.removeClass("scrittaAttiva");
       doveSono3 = $(this);
       doveSono3.addClass("scrittaAttiva");
-     // banneroneTrasformati.to(0); TODO: Qui ci deve andare il cambio di immagine
+      $("#ImmagineSection3").attr("src","./materiale/TRASFORMATI/GRIGLIATI.png");
       titoloBoxTrasformati.text("Le verdure grigliate");
       testoBoxTrasformati.text("I grigliati presentano un sapore rustico, arricchito da un bilanciato mix di spezie mediterranee. Sono gustosi antipasti o contorni ai secondi piatti a base di carne o di pesce.");
     });
@@ -146,7 +146,7 @@ $('#fullpage').fullpage({
       doveSono3.removeClass("scrittaAttiva");
       doveSono3 = $(this);
       doveSono3.addClass("scrittaAttiva");
-     // banneroneTrasformati.to(1); TODO: Qui ci deve andare il cambio di immagine
+      $("#ImmagineSection3").attr("src","./materiale/TRASFORMATI/COMPOSTE.png");
       titoloBoxTrasformati.text("Le composte");
       testoBoxTrasformati.text("Le composte di prugne e peperoncino e prugne con chiodi di garofano vengono realizzate con la varietà Stanley. Il sapore fresco delle prugne con il piccante del peperoncino è ideale per accompagnare formaggi stagionati e carni rosse. Quella con chiodi di garofano ha un sapore esotico e delicato, ottima da spalmare sul pane o su fette biscottate. La composta di cipolle si caratterizza per il sapore agrodolce e speziato. Si accoppia con i formaggi stagionati, le carni ed i vini rossi.");
     });
@@ -155,7 +155,7 @@ $('#fullpage').fullpage({
       doveSono3.removeClass("scrittaAttiva");
       doveSono3 = $(this);
       doveSono3.addClass("scrittaAttiva");
-     // banneroneTrasformati.to(2); TODO: Qui ci deve andare il cambio di immagine
+      $("#ImmagineSection3").attr("src","./materiale/TRASFORMATI/PATE.png");
       titoloBoxTrasformati.text("I Paté");
       testoBoxTrasformati.text("Fatti con gli ingredienti semplici della tradizione mediterranea, i paté di zucchine, di melanzane, di carciofi, di peperoni e di ceci sono ideali per insaporire bruschette ed accompagnare secondi piatti.");
     });
@@ -164,7 +164,7 @@ $('#fullpage').fullpage({
       doveSono3.removeClass("scrittaAttiva");
       doveSono3 = $(this);
       doveSono3.addClass("scrittaAttiva");
-    //  banneroneTrasformati.to(3); TODO: Qui ci deve andare il cambio di immagine
+      $("#ImmagineSection3").attr("src","./materiale/TRASFORMATI/SUGO_OLIVE.png");
       titoloBoxTrasformati.text("Sugo e Olive verdi");
       testoBoxTrasformati.text("Il sugo mediterraneo viene preparato secondo la tradizione culinaria siciliana. Si gusta come condimento per la pasta o per le bruschette. Prodotte sulle colline di trapani, le olive verdi sono ottime come snack o per accompagnare un cocktail.");
     });
